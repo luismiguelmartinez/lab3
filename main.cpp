3edacad97c8cf2c88f682ae7883ba0e364b4fc04
Luis Miguel Martinez Ocampo <miguel8515@gmail.com>
2020-10-27 20:22:48 -0500

    Desarrollo de funcion de conversion a binario y definicion de funcion de codificacion de datos

--- main.cpp
+++ main.cpp
@@ -4,53 +4,99 @@
 
 using namespace std;
 
-void write ();
-void toBinary (string, char *, unsigned short *);
+void encode (string *, string *, short *, short *);
+void toBinary (ifstream *, ofstream *);
+void encodeBinaryData (ifstream *, ofstream *, short *, short *);
 
 int main()
 {
-    write();
+    string input, output;
+    short n, method;
+
+    cout << "Ingrese nombre archivo de entrada" << endl;
+    //getline(cin, input);
+    input = "original";
+
+    cout << "Ingrese nombre archivo de salida" << endl;
+    //getline(cin, output);
+    output = "codificado";
+
+    cout << "Ingrese semilla n" << endl;
+    //cin >> n;
+
+    cout << "Ingrese metodo de codificacion (1 o 2)" << endl;
+    //cin >> method;
+
+    encode(&input, &output, &n, &method);
 
     return 0;
 }
 
-void write () {
+void encode (string *input, string *output, short *n, short *method) {
 
     try {
-        ofstream fout;
-        fout.open("temp2.txt");
-
-        if (!fout.is_open())
+        ifstream fin;
+        fin.open(*input + ".txt");
+        if (!fin.is_open())
             throw '1';
 
-        cout << "Ingrese una palabra" << endl;
-        string buffer;
-        cin >> buffer;
-        char binary[9];
-        memset(binary, '0', 8);
+        ofstream fout;
+        fout.open("binary.txt");
+        if (!fout.is_open())
+            throw '2';
 
-        for (unsigned short i = 0; i < buffer.length(); i++) {
-            toBinary(buffer, binary, &i);
-            fout << binary;
-        }
+        toBinary(&fin, &fout);
+
+
+        fout.open("binary.txt");
+        if (!fin.is_open())
+            throw '1';
+
+        fout.open(*output + ".txt");
+        if (!fout.is_open())
+            throw '2';
+
+
+
+
+        fin.close();
         fout.close();
 
 
     }  catch (char c) {
         if (c == '1')
+            cout << "e: Error archivo de lectura" << endl;
+        else if (c == '2') {
             cout << "e: Error archivo de escritura" << endl;
+        }
         else
             cout << "e: Error inesperado" << endl;
     }
 }
 
-void toBinary(string word, char *auxBinary, unsigned short *i) {
-    short aux = word[*i];
+void toBinary(ifstream *fin, ofstream *fout) {
+    short j;
+    short aux;
 
-    short j = 0;
-    while (aux > 0) {
-        *(auxBinary + 7 - j) = char(aux % 2) + 48;
-        j++;
-        aux /= 2;
+    char binary[9];
+    memset(binary, '0', 8);
+
+    fin->seekg(0);
+
+    while (fin->good()) {
+        j = 0;
+        aux = fin->get();
+
+        if (fin->good()) {
+            while (aux > 0) {
+                binary[7 - j++] = char(aux % 2) + 48;
+                aux /= 2;
+            }
+            *fout << binary;
+        }
     }
+}
+
+void encodeBinaryData (ifstream *fin, ofstream *fout, short *n, short *method) {
+
 }
